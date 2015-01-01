10b797457d341f7b5d1b2e786909f2bd8c32685b
diptin <dipti.nirmale@gmail.com>
2014-12-31 18:54:42 -0800

    Added test suite for Strings tests

---
+++ test/org/diptin/strings/tests/StringsTestSuite.java
@@ -0,0 +1,13 @@
+package org.diptin.strings.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	StringPermuterTest.class, 
+	StringSegregatorTest.class })
+
+public class StringsTestSuite {
+}
